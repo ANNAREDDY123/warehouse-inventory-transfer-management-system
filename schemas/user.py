5bb11fe39cc388849e194306c94e1822316a0d2c
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-07-15 16:55:25 +0530

    Create user.py

---
+++ schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    Field
+)
+
+
+class UserCreate(BaseModel):
+
+    username: str = Field(..., min_length=3)
+
+    email: EmailStr
+
+    password: str = Field(..., min_length=6)
+
+    role: str
+
+
+class UserLogin(BaseModel):
+
+    email: EmailStr
+
+    password: str
